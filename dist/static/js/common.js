3d916ebc19e921e421e26d1e911dc5f3a2c38d8e
czero1995 <czero1995@gmail.com>
2018-07-01 10:41:50 +0800

    change service config

--- dist/static/js/common.js
+++ dist/static/js/common.js
@@ -1,3 +1,3 @@
 export const phoneReg = /(1[3-9]\d{9}$)/;
-// export const base_url = 'http://infinite.czero.cn:3000/api/';
-export const base_url = 'api/';
+export const base_url = 'http://infinite.czero.cn:3000/api/';
+// export const base_url = 'api/';
